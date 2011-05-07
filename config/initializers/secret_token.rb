dd44f83bf5b72ad70af9451377c80d1df99e381d
Aitor García <aitor.garcia@gmail.com>
2011-05-07 02:31:43 +0000

    Initial layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Verbim::Application.config.secret_token = 'cd700f80501edc5d0eaa7fc730db52841ca4222b4b460058785b540f0de7c1b327c89da6d200a81cc1d3ea7f7ba4d036ef26bfb791ba0d5588373a87e6de9dca'
