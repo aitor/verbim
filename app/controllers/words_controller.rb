13dfa411f878825161727cd6191e8392dbba024a
Aitor García <aitor.garcia@gmail.com>
2011-05-08 08:37:33 +0000

    Fixed typo

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,7 +1,7 @@
 class WordsController < ApplicationController
 
   def show
-    @word = Word.find_by_name params[:id])
+    @word = Word.find_by_name params[:id]
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml  => @word.to_xml(:except => [:html, :id]) }
